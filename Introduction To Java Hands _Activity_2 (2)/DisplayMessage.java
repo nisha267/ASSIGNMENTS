f728d0f7de4a49d1f66087562978d1c021de64ab
Nisha Sutradhar <111556178+nisha267@users.noreply.github.com>
2022-09-30 12:46:14 +0530

    Add files via upload

---
+++ Introduction To Java Hands _Activity_2 (2)/DisplayMessage.java
@@ -0,0 +1,14 @@
+package com.cognizant.shapes;
+
+public class DisplayMessage {
+	
+		  void printMessage(){
+		   System.out.println("Hello My Message");
+		}
+		}
+
+
+  
+//Save the file as “DisplayMessage.java”  .
+
+
---
+++ Introduction To Java Hands _Activity_2 (2)/MyProgram.java
@@ -0,0 +1,8 @@
+package com.cognizant.shapes;
+
+ class MyProgram{
+	 public static void main(String[] args) {
+		DisplayMessage d=new DisplayMessage();
+		d.printMessage();
+	}
+ }
