7b0ba2106fe50f51a3af894f7103a08007752071
Nisha Sutradhar <111556178+nisha267@users.noreply.github.com>
2022-09-30 11:52:30 +0530

    Add files via upload

---
+++ Statements_In_Java_Activity_1 (1)/MyProgram.java
@@ -0,0 +1,18 @@
+package com.cognizant.shapes;
+
+ class Ifelse  {
+  
+    public static void main(String arg[]){
+    	int num=4;
+    	if(num==0)
+    		  System.out.println("Can't determine");
+    		 
+    else
+    			 System.out.println("Showing.......");
+    
+    if(num%2==0)
+    	System.out.println("Even number");
+    else 
+    	System.out.println("Odd number");
+    } 	}	
+
